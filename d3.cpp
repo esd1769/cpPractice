f6586533ad0b9db693aba682fff41a2c21f4733c
em3459 <119pubgwarrior@gmail.com>
2025-05-22 18:42:54 -0700

    Update d3.cpp

--- d3.cpp
+++ d3.cpp
@@ -1,3 +1,21 @@
+// class Solution {
+// public:
+//     int reverse(int x) {
+//        // int negcheck=1;
+//         //if (x<0) negcheck =-1;
+//         //x=x*negcheck;
+//         long long ans=0;
+//         while (x!=0){
+//             int lastdigit=x%10;
+//             x=x/10;
+//             ans=(ans*10)+lastdigit;
+//         }
+//         if (ans > INT_MAX || ans < INT_MIN) {
+//                 return 0;
+//             }
+//         return (int) ans;
+//     }
+// };
 class Solution {
 public:
     int reverse(int x) {
@@ -8,10 +26,12 @@ public:
         while (x!=0){
             int lastdigit=x%10;
             x=x/10;
+
             ans=(ans*10)+lastdigit;
-        }
-        if (ans > INT_MAX || ans < INT_MIN) {
+             if (ans > INT_MAX || ans < INT_MIN) {
                 return 0;
+        }
+       
             }
         return (int) ans;
     }
