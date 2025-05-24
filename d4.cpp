e1941b3dd16c8b321c88fad9ecfe12b549e5a33f
em3459 <119pubgwarrior@gmail.com>
2025-05-24 09:54:50 -0700

    Create d4.cpp

---
+++ d4.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+        bool isPalindrome(int x)  {
+            if (x<0) return false;
+        int orgNum=x;
+        
+       // int negcheck=1;
+        //if (x<0) negcheck =-1;
+        //x=x*negcheck;
+        long long ans=0;
+        while (x!=0){
+            int lastdigit=x%10;
+            x=x/10;
+
+            ans=(ans*10)+lastdigit;
+             if (ans > INT_MAX || ans < INT_MIN) {
+                return 0;
+        }
+       
+            }
+        return (int)ans==orgNum;
+    }
+};
