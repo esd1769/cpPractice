6b4d20744ee34877c195725d17276232b958a0aa
em3459 <119pubgwarrior@gmail.com>
2025-05-28 09:37:46 -0700

    Create d8clean.cpp

---
+++ d8clean.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        bool neg=true;
+        if (dividend <0 && divisor > 0) neg=false;
+        if (dividend >0 && divisor < 0) neg=false;
+        long n = abs((long)dividend);
+        long d = abs((long)divisor);
+
+        long ans=0;
+        while(n >= d){
+            int c=0;
+            while (n>(d<<(c+1))){
+                c++;
+            }
+            
+            ans=ans+(1<<c);
+            n=n-(d<<c);
+        }
+        if (neg && ans >= INT_MAX) return INT_MAX;
+        if (!neg && -ans <= INT_MIN) return INT_MIN;
+        return neg ? ans : -ans;
+        
+    }
+};
