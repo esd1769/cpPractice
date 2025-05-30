222caf78e4d418fb4c92b4fa5acbdfe142233ede
em3459 <119pubgwarrior@gmail.com>
2025-05-30 09:14:33 -0700

    Update d10.cpp

--- d10.cpp
+++ d10.cpp
@@ -4,8 +4,9 @@ public:
         
         int n=nums.size();
         for(int i=0;i<n;i++){
-            for (int j=0;j<n;j++){
-                if (i==j) continue;
+            // for (int j=0;j<n;j++){
+            for (int j=1;j<n;j++){
+                //if (i==j) continue;
                 if (nums[i]+nums[j]==target){ return {i, j} ;}
 
             }
