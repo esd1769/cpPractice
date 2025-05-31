515c25b75fd9e263840a6ed7f514f222d16601a8
em3459 <119pubgwarrior@gmail.com>
2025-05-31 09:10:58 -0700

    Update d11.cpp

--- d11.cpp
+++ d11.cpp
@@ -9,10 +9,14 @@ public:
             if( nums[i]==0 )c0++;
             else if (nums[i]==1) c1++;
             else if (nums[i]==2) c2++;}
-        int index=0;
-        for (int i=0;i<c0;i++){nums[index++]=0;}
-        for (int i=0;i<c1;i++){nums[index++]=1;}
-        for (int i=0;i<c2;i++){nums[index++]=2;}
+        // int index=0;
+        // for (int i=0;i<c0;i++){nums[index++]=0;}
+        // for (int i=0;i<c1;i++){nums[index++]=1;}
+        // for (int i=0;i<c2;i++){nums[index++]=2;}
+        
+        for (int i=0;i<c0;i++){nums[i]=0;}
+        for (int i=c0;i<c1+c0;i++){nums[i]=1;}
+        for (int i=c1+c0;i<c2+c1+c0;i++){nums[i]=2;}
        
     }
 };
