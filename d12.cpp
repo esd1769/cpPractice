52c92d7afb935894a93837e72291bb5763304c25
em3459 <119pubgwarrior@gmail.com>
2025-06-01 09:41:10 -0700

    Update d12.cpp

--- d12.cpp
+++ d12.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int n=nums.size();
-        for (int i=0;i<n;i++){
-            int count=0;
-            for (int j=0;j<n;j++){
-                if (nums[i]==nums[j])count++;
-            }
-            if (count>n/2) return nums[i];
-        }
-        return 0;
+    //     int n=nums.size();
+    //     for (int i=0;i<n;i++){
+    //         int count=0;
+    //         for (int j=0;j<n;j++){
+    //             if (nums[i]==nums[j])count++;
+    //         }
+    //         if (count>n/2) return nums[i];
+    //     }
+    //     return 0;
+
+    
+std::vector<std::pair<int, int>> mpp;
+    for (int i=0;i<nums.size();i++){
+        mpp[nums[i]].second++;
+    }
+     for (int i=0;i<nums.size();i++){
+        if (mpp[i].second>nums.size()/2) return mpp[i].first;
+    }
+    return -1;
     }
 };
