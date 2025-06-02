009a1f1849dc831aa1e1c40876ed03f0440a1812
em3459 <119pubgwarrior@gmail.com>
2025-06-02 10:01:04 -0700

    Update d13.cpp

--- d13.cpp
+++ d13.cpp
@@ -1,4 +1,4 @@
 //Kadane's Algorithm | Maximum Subarray Sum
 //[1,0,-1,2,4]
-//subarray-consiquent part[1,0,-1]...not [1,-1]
+//subarray-consiquative part[1,0,-1]...not [1,-1]
 //sub sequence....[1,-1]
