68dde0960e9a408f7d8f0d6e9dc14bf0d1722c52
em3459 <119pubgwarrior@gmail.com>
2025-06-02 10:27:38 -0700

    Update d13.cpp

--- d13.cpp
+++ d13.cpp
@@ -2,6 +2,7 @@
 //[1,0,-1,2,4]
 //subarray-consiquative part[1,0,-1]...not [1,-1]
 //sub sequence....[1,-1]
+//maxi=max(maxi,sum);...comma,not semicolon in max min
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
@@ -19,3 +20,19 @@ public:
         
     }
 };
+//         int maxi=INT_MIN;
+//         for (int i=0;i<nums.size();i++){
+//             for(int j=i;j<nums.size();j++){
+//                 int sum=0;
+//                 for (int k=i;k<=j;k++){
+//                     sum+=nums[k]; 
+//                 }
+//                 maxi=max(maxi,sum);
+//             }
+//         }
+//         return maxi;
+        
+//     }
+   
+    
+// };
