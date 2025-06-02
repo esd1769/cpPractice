4dd10bc218589be9c831a96e3d103e4a77af642f
em3459 <119pubgwarrior@gmail.com>
2025-06-02 10:15:00 -0700

    Update d13.cpp

--- d13.cpp
+++ d13.cpp
@@ -2,3 +2,20 @@
 //[1,0,-1,2,4]
 //subarray-consiquative part[1,0,-1]...not [1,-1]
 //sub sequence....[1,-1]
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        long long maxi=INT_MIN;
+        long long sum=0;
+        for (int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            
+           
+            if (maxi<sum)maxi=sum;
+            if (sum<0)sum=0;
+            
+        }
+        return (int)maxi;
+        
+    }
+};
