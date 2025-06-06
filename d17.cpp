e7c33b48d39e407e12d66c9d986e998d547fe221
em3459 <119pubgwarrior@gmail.com>
2025-06-06 09:47:35 -0700

    Update d17.cpp

--- d17.cpp
+++ d17.cpp
@@ -1,2 +1,3 @@
 //sorted+find=binary search
-// WHY {4low....2mid.....3}OKAY...[4low...4....2mid...3]
+// WHY {4low....2mid.....3}OKAY...[4low...4....2mid...3]NOT
+//CUZ rotated array have only one low>mid case
