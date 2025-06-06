70186e33b041be1c5fa3bdc95f25fb0d376702ee
em3459 <119pubgwarrior@gmail.com>
2025-06-06 10:09:46 -0700

    Update d17.cpp

--- d17.cpp
+++ d17.cpp
@@ -2,3 +2,4 @@
 // WHY {4low....2mid.....3}OKAY...[4low...4....2mid...3]NOT
 //CUZ rotated array have only one low>mid case
 //if more than one...if is not sorted anymore 
+//the part where the rotating point is,has minimum
