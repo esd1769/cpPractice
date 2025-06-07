b64463e0efacc77b369f18c7840c18dbeb139a80
em3459 <119pubgwarrior@gmail.com>
2025-06-07 09:09:55 -0700

    Update d18.cpp

--- d18.cpp
+++ d18.cpp
@@ -1,3 +1,4 @@
 //whenever...got sorted..we can use binary search
 //binary search..not only in array..but for everything
 //bs-everything like dictionary,books,texts,string also
+//when we have lots of edge cases..we use triming parts by bs
