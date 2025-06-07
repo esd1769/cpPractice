5e620149796c50a38deb213c57b8a14694f5ba79
em3459 <119pubgwarrior@gmail.com>
2025-06-07 09:47:10 -0700

    Update d18.cpp

--- d18.cpp
+++ d18.cpp
@@ -26,7 +26,7 @@ public:
         while(low<=high){
             mid=low+((high-low)/2);
             if ((nums[mid-1]!=nums[mid])&&(nums[mid]!=nums[mid+1])) return nums[mid];
-            else if (((mid%2==1)&&(nums[mid-1]==nums[mid]))||((mid%2==0)&&(nums[mid+1]==nums[mid]))) low=mid+1;
+            else if (((mid%2==1)&&(nums[mid-1]==nums[mid]))||((mid%2==0)&&(nums[mid+1]==nums[mid]))) {}
             else{high=mid-1;}
         }
         return -1;//dummy..it will return already from inside of  while looop
