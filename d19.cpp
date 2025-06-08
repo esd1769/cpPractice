d30fcce609bb3b24a769285d67634311a499af96
em3459 <119pubgwarrior@gmail.com>
2025-06-08 10:52:16 -0700

    Update d19.cpp

--- d19.cpp
+++ d19.cpp
@@ -6,3 +6,22 @@
 //IF FUNCTION has return type int...you need to give to give hypothetical return -1
 //controlling edge case more efficiently
 //comes from experience
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n=nums.size(),low=1,high=n-2,mid=0;
+        if (n==1)return 0;
+        if (nums[n-1]>nums[n-2]) return n-1;
+        if (nums[0]>nums[1]) return 0; 
+        while(low<=high){
+            mid=low+((high-low)/2);
+            if ((nums[mid]>nums[mid-1])&&(nums[mid]<nums[mid+1]))return mid;
+            else if (nums[mid]>nums[mid-1]) low=mid+1;
+            else{ high=mid-1;}
+
+
+        }
+        return -1;
+        
+    }
+};
