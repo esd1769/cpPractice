51e8be792e1169c3b26989e25ea95d3ba66806e1
em3459 <119pubgwarrior@gmail.com>
2025-06-08 10:54:09 -0700

    Update d19.cpp

--- d19.cpp
+++ d19.cpp
@@ -15,7 +15,7 @@ public:
         if (nums[0]>nums[1]) return 0; 
         while(low<=high){
             mid=low+((high-low)/2);
-            if ((nums[mid]>nums[mid-1])&&(nums[mid]<nums[mid+1]))return mid;
+            if ((nums[mid]>nums[mid-1])&&(nums[mid]>nums[mid+1]))return mid;
             else if (nums[mid]>nums[mid-1]) low=mid+1;
             else{ high=mid-1;}
 
