d8c0bc17a05f3696732d8c390d2be91344392aa4
em3459 <119pubgwarrior@gmail.com>
2025-06-10 09:41:40 -0700

    Update d21.cpp

--- d21.cpp
+++ d21.cpp
@@ -1,5 +1,17 @@
 //general loop O(n)
 //try to covert loop to bs for O(logN) 
+//WHEN WE SEE PATERN..IT is binary search
+//xxxxxxxxxxxxxxxxxxxxxxxxxxxx000000000000000000000000000000000
+//not possible                minimulpossible         allpossible 
+//low                             mid                     high
+
+
+//so binary search
+//whenever i get 3parts...i can replace 
+//linear search....... to.........binary search
+//O(n).................to..........O(logN)
+//range matters...for time complexity
+//so try to minimize the range
 
 
 if (m*k>n)return -1;
