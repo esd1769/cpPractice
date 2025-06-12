9b5e34502a7fb5b26c6a9bbb1da4b532abb40548
em3459 <119pubgwarrior@gmail.com>
2025-06-12 11:53:14 -0700

    Create d23.cpp

---
+++ d23.cpp
@@ -0,0 +1,50 @@
+class Solution {
+private:
+int lb(vector<int>& nums, int target){
+    int low=0,n=nums.size(),high=n-1,mid=0,first=-1,last=-1;
+        while (low<=high){
+            mid=low+(high-low)/2;
+            if (target==nums[mid]){
+                first=mid;
+                high=mid-1;
+            }
+            else if(nums[mid]<target){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return first;
+
+}
+int ub(vector<int>& nums, int target){
+    int low=0,n=nums.size(),high=n-1,mid=0,first=-1,last=-1;
+        while (low<=high){
+            mid=low+(high-low)/2;
+            if (target==nums[mid]){
+                first=mid;
+                low=mid+1;
+            }
+            else if(nums[mid]<target){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return first;
+
+}
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int first=-1,last=-1;
+        first=lb(nums,target);
+        if (first==-1) return {-1,-1};
+        last=ub(nums,target);
+        return {first,last};
+        
+        
+        
+    }
+};
