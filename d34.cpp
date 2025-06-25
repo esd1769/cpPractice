82a2fa4b302daf6367fa7912984bad1ea6910e31
em3459 <119pubgwarrior@gmail.com>
2025-06-25 10:37:26 -0700

    Create d34.cpp

---
+++ d34.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        return maxDepthMod(root)!=-1;
+        
+        
+    }
+    int maxDepthMod(TreeNode* root) {
+        if (root==NULL) return 0;
+        int left=maxDepthMod(root->left);
+        if (left==-1)return -1;
+        
+        int right=maxDepthMod(root->right);
+        if (right==-1)return -1;
+        if (abs(left-right))return -1;
+        return 1+max(left,right);
+        
+    }
+};
