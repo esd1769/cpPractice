980b69dd17268664727be7f4a8235c38bd7be70c
em3459 <119pubgwarrior@gmail.com>
2025-06-25 10:44:26 -0700

    Update d34.cpp

--- d34.cpp
+++ d34.cpp
@@ -23,7 +23,7 @@ public:
         
         int right=maxDepthMod(root->right);
         if (right==-1)return -1;
-        if (abs(left-right))return -1;
+        if (abs(left-right)>1)return -1;
         return 1+max(left,right);
         
     }
