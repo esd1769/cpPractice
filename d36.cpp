1ddebca30e752d7e49e5d5eead816a6108a97303
em3459 <119pubgwarrior@gmail.com>
2025-06-28 11:15:55 -0700

    Update d36.cpp

--- d36.cpp
+++ d36.cpp
@@ -10,7 +10,8 @@
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  * 
  * 
- *     TreeNode(int x) : val(x),  left(nullptr),
+ *     TreeNode(int x) :
+ *  val(x),  left(nullptr),
  *  right(nullptr) {}
  *     TreeNode(int x, TreeNode *left , TreeNode *right)  : val(x), left(left ), right(right) {}
  * };
